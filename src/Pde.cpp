45aa71001b6963a6b5290dfdc5899a3f1d2f9a50
martinjrobins <martinjrobins@gmail.com>
2013-02-09 03:37:52 +0000

    added Pde and Species test classes and cmake files to compile to python modules

---
+++ src/Pde.cpp
@@ -0,0 +1,37 @@
+/* 
+ * Pde.cpp
+ *
+ * Copyright 2012 Martin Robinson
+ *
+ * This file is part of PDE_BD.
+ *
+ * PDE_BD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PDE_BD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PDE_BD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Created on: Feb 9, 2013
+ *      Author: mrobins
+ */
+
+#include "Pde.h"
+#include <iostream>
+
+Pde::Pde() {
+	std::cout << "Contructing pde" << std::endl;
+
+}
+
+void Pde::integrate(const double dt) {
+	std::cout << "integrating for "<<dt<<" seconds." << std::endl;
+}
+
+
---
+++ src/Species.h
@@ -0,0 +1,33 @@
+/* 
+ * Species.h
+ *
+ * Copyright 2012 Martin Robinson
+ *
+ * This file is part of PDE_BD.
+ *
+ * PDE_BD is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PDE_BD is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with PDE_BD.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Created on: Feb 9, 2013
+ *      Author: mrobins
+ */
+
+#ifndef SPECIES_H_
+#define SPECIES_H_
+
+class Species {
+public:
+	Species();
+};
+
+#endif /* SPECIES_H_ */
