8b63e7a6d1f0f9f7a1edc47275ce6626ea0543cc
martinjrobins <martinjrobins@gmail.com>
2013-02-13 16:13:35 +0000

    moved stuff from trilinosRD to pde

--- src/Pde.h
+++ src/Pde.h
@@ -25,15 +25,116 @@
 #ifndef PDE_H_
 #define PDE_H_
 
-struct MyTrilinosData;
+#include "Tpetra_DefaultPlatform.hpp"
+#include "Tpetra_CrsMatrix.hpp"
+#include "Tpetra_Vector.hpp"
+#include "Teuchos_FancyOStream.hpp"
+#include "Teuchos_ScalarTraits.hpp"
+#include "Teuchos_XMLParameterListHelpers.hpp"
+
+
+// Intrepid includes
+#include <Intrepid_FunctionSpaceTools.hpp>
+#include <Intrepid_CellTools.hpp>
+#include <Intrepid_ArrayTools.hpp>
+#include <Intrepid_HGRAD_HEX_C1_FEM.hpp>
+#include <Intrepid_RealSpaceTools.hpp>
+#include <Intrepid_DefaultCubatureFactory.hpp>
+#include <Intrepid_Utils.hpp>
+
+// Teuchos includes
+//#include <Teuchos_TimeMonitor.hpp>
+
+// Shards includes
+#include <Shards_CellTopology.hpp>
+
+// Pamgen includes
+#include <create_inline_mesh.h>
+#include <im_exodusII_l.h>
+#include <im_ne_nemesisI_l.h>
+#include <pamgen_extras.h>
+
+// Sacado includes
+#include <Sacado.hpp>
+
+
 
 class Pde {
+	typedef double ST;
+	typedef int    LO;
+	typedef int    GO;
+	typedef Tpetra::DefaultPlatform::DefaultPlatformType::NodeType  Node;
+
+
+	typedef Tpetra::CrsMatrix<ST, LO, GO, Node>    sparse_matrix_type;
+	typedef Tpetra::Operator<ST, LO, GO, Node>     operator_type;
+	typedef Tpetra::MultiVector<ST, LO, GO, Node>  multivector_type;
+	typedef Tpetra::Vector<ST, LO, GO, Node>       vector_type;
+
 public:
 	Pde(const char* filename);
 	void integrate(const double dt);
+	//static void init();
 private:
-	MyTrilinosData data;
-	Teuchos::GlobalMPISession& mpiSession
+	RCP<sparse_matrix_type> A,B;
+	RCP<vector_type> X;
+	Teuchos::RCP<const Comm<int> > comm;
+	Teuchos::RCP<Node> node;
+
+	/** \brief  User-defined material tensor.
+
+	    \param  material    [out]   3 x 3 material tensor evaluated at (x,y,z)
+	    \param  x           [in]    x-coordinate of the evaluation point
+	    \param  y           [in]    y-coordinate of the evaluation point
+	    \param  z           [in]    z-coordinate of the evaluation point
+
+	    \warning Symmetric and positive definite tensor is required for every (x,y,z).
+	*/
+	template<typename Scalar>
+	void materialTensor (Scalar material[][3],
+	                const Scalar& x,
+	                const Scalar& y,
+	                const Scalar& z);
+
+	/** \brief Compute the material tensor at array of points in physical space.
+
+	    \param worksetMaterialValues      [out]     Rank-2, 3 or 4 array with dimensions (C,P), (C,P,D) or (C,P,D,D)
+	                                                with the values of the material tensor
+	    \param evaluationPoints           [in]      Rank-3 (C,P,D) array with the evaluation points in physical frame
+	*/
+	template<class ArrayOut, class ArrayIn>
+	void evaluateMaterialTensor (ArrayOut& worksetMaterialValues,
+	                        const ArrayIn& evaluationPoints);
+	/** \brief Computes source term: f
+
+	    \param  x          [in]    x-coordinate of the evaluation point
+	    \param  y          [in]    y-coordinate of the evaluation point
+	    \param  z          [in]    z-coordinate of the evaluation point
+
+	    \return Source term corresponding to the user-defined exact solution evaluated at (x,y,z)
+	 */
+	template<typename Scalar>
+	const Scalar sourceTerm (Scalar& x, Scalar& y, Scalar& z);
+
+	void
+	makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
+	                            Teuchos::RCP<vector_type>& B,
+	                            Teuchos::RCP<vector_type>& X_exact,
+	                            Teuchos::RCP<vector_type>& X,
+	                            const Teuchos::RCP<const Teuchos::Comm<int> >& comm,
+	                            const Teuchos::RCP<Node>& node,
+	                            const std::string& meshInput);
+
+	//! Just like above, but with multivector_type output arguments.
+	void
+	makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
+	                            Teuchos::RCP<multivector_type>& B,
+	                            Teuchos::RCP<multivector_type>& X_exact,
+	                            Teuchos::RCP<multivector_type>& X,
+	                            const Teuchos::RCP<const Teuchos::Comm<int> >& comm,
+	                            const Teuchos::RCP<Node>& node,
+	                            const std::string& meshInput);
+
 };
 
 
